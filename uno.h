010e85ea56a35cbb0ec0c7b476b1c4e96afc1f17
byebay <bayuuua00@gmail.com>
2025-05-19 21:26:37 +0700

    mengubah struct dan menambahkan beberapa modul dan fungsi (silahkan tambah atau hapus modul/fungsinya jika perlu)

--- uno.h
+++ uno.h
@@ -1,20 +1,20 @@
-#ifndef uno_H
-#define uno_H
+#ifndef UNO_H
+#define UNO_H
+
 #include <stdio.h>
 #include "adt-queue/queue.h"
 #include "adt-stack/STACK.h"
-#include "adt-linked-list/SpNRSLL.H"
+#include "adt-linked-list/SpNRSLL.h"
 
-typedef enum
-{
+typedef enum {
     MERAH,
     HIJAU,
     BIRU,
     KUNING,
     HITAM
 } WarnaKartu;
-typedef enum
-{
+
+typedef enum {
     ANGKA,
     SKIP,
     REVERSE,
@@ -23,38 +23,59 @@ typedef enum
     WILD,
     WILD_DRAW4
 } JenisKartu;
-typedef char infotype;
-typedef struct Kartu *addresskartu;
-typedef struct KartuTree *addresstree;
-typedef struct KartuList *addresslist;
-typedef struct Player *addresspemain;
-typedef struct
-{
+
+typedef struct {
+    // Hapus/ubah aja kalo gk perlu
     WarnaKartu warna;
     JenisKartu jenis;
-    int angka;
+    int angka; // Hanya jika jenis == ANGKA
 } Kartu;
 
-typedef struct
-{
-    addresskartu info;
-    addresstree left;
-    addresstree right;
+typedef struct KartuTree {
+    Kartu info;
+    struct KartuTree *left;
+    struct KartuTree *right;
 } KartuTree;
 
-typedef struct
-{
-    addresskartu info;
-    addresslist next;
+typedef struct KartuList {
+    Kartu info;
+    struct KartuList *next;
 } KartuList;
 
-typedef struct
-{
+typedef struct Player {
     char nama[50];
-    addresskartu tangan;
+    KartuList *tangan;
     int jumlahKartu;
     int sudahUndoRedo;
-    addresspemain next;
+    struct Player *next;
 } Player;
+
+void InitDeck(List *deck);
+void ShuffleDeck(List *deck);
+infotype DrawCard(List *deck);
+void AddCardToDeck(List *deck, infotype kartu);
+void InitGiliran(Queue *q, int jumlahPemain);
+void NextGiliran(Queue *q);
+int CurrentPlayer(Queue q);
+void InitDiscard(Stack *s);
+void PushDiscard(Stack *s, infotype kartu);
+infotype TopDiscard(Stack s);
+infotype PopDiscard(Stack *s);
+void CreatePlayerList(Player **head);
+Player *AlokasiPlayer(const char *nama);
+void DealokasiPlayer(Player *P);
+int PlayerListEmpty(Player *head);
+void InsertPlayerFirst(Player **head, Player *P);
+Player *SearchPlayer(Player *head, const char *nama);
+int DeletePlayer(Player **head, const char *nama);
+void PrintPlayerList(Player *head);
+KartuList *AlokasiKartu(Kartu k);
+void DealokasiKartu(KartuList *P);
+int KartuListEmpty(KartuList *head);
+void InsertKartuFirst(KartuList **head, KartuList *P);
+void InsertKartuLast(KartuList **head, KartuList *P);
+int DeleteKartu(KartuList **head, Kartu kartu);
+int CountKartu(KartuList *head);
+void PrintKartuList(KartuList *head);
 
 #endif
