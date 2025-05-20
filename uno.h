bc963107299ba9f9e5b87b6ec28fa3d0d36d65b6
Zidan <zidantaufiqurahman@gmail.com>
2025-05-20 12:58:51 +0700

    memberikan penjelasan fungsi pada uno.h

--- uno.h
+++ uno.h
@@ -51,31 +51,66 @@ typedef struct Player {
 } Player;
 
 void InitDeck(List *deck);
+// Fungsi untuk menginisialisasi deck kartu
+// dengan 108 kartu UNO
+// 76 kart u angka (0-9) dari 4 warna
+// 8 kartu SKIP dari 4 warna
+// 8 kartu REVERSE dari 4 warna
+// 8 kartu DRAW2 dari 4 warna
+// 4 kartu WILD
+// 4 kartu WILD DRAW4
 void ShuffleDeck(List *deck);
+// Fungsi untuk mengacak urutan kartu dalam deck
 infotype DrawCard(List *deck);
+// Fungsi untuk menarik kartu dari deck jika tidak yang di mainin
 void AddCardToDeck(List *deck, infotype kartu);
+// Fungsi untuk menambah kartu ke deck
 void InitGiliran(Queue *q, int jumlahPemain);
+// Fungsi untuk menginisialisasi giliran pemain
+// dengan memasukkan pemain ke dalam queue
 void NextGiliran(Queue *q);
+// fungsi untuk mengubah giliran pemain
 int CurrentPlayer(Queue q);
+// Fungsi untuk mendapatkan pemain yang sedang bermain dari queue
 void InitDiscard(Stack *s);
+// Fungsi untuk menginisialisasi  stack kartu yang dimainin
 void PushDiscard(Stack *s, infotype kartu);
+// Fungsi untuk menambah kartu ke stack kartu yang dimainin
 infotype TopDiscard(Stack s);
+// fungsi untuk melihat kartu yang ada di atas stack kartu yang dimainin
 infotype PopDiscard(Stack *s);
+// fungsi untuk mengeluarkan kartu dari stack untuk undo
 void CreatePlayerList(Player **head);
+// Fungsi untuk membuat list pemain
 Player *AlokasiPlayer(const char *nama);
+// Fungsi untuk mengalokasi memori untuk pemain baru
 void DealokasiPlayer(Player *P);
+// Fungsi untuk dealokasi memori pemain jika sudah tidak digunakan
 int PlayerListEmpty(Player *head);
+// Fungsi untuk mengecek apakah list pemain kosong atau tidak
 void InsertPlayerFirst(Player **head, Player *P);
+// Fungsi untuk menambah pemain ke list pemain
 Player *SearchPlayer(Player *head, const char *nama);
+// Fungsi untuk mencari pemain dalam list pemain
 int DeletePlayer(Player **head, const char *nama);
+// Fungsi untuk menghapus pemain dari list pemain
 void PrintPlayerList(Player *head);
+// Fungsi untuk mencetak list pemain
 KartuList *AlokasiKartu(Kartu k);
+// Fungsi untuk mengalokasi memori untuk kartu baru
 void DealokasiKartu(KartuList *P);
+// Fungsi untuk dealokasi memori kartu jika sudah tidak digunakan
 int KartuListEmpty(KartuList *head);
+// Fungsi untuk mengecek apakah list kartu kosong atau tidak
 void InsertKartuFirst(KartuList **head, KartuList *P);
+// Fungsi untuk menambah kartu ke list kartu
 void InsertKartuLast(KartuList **head, KartuList *P);
+// Fungsi untuk menambah kartu ke list kartu
 int DeleteKartu(KartuList **head, Kartu kartu);
+// Fungsi untuk menghapus kartu dari list kartu
 int CountKartu(KartuList *head);
+// Fungsi untuk menghitung jumlah kartu dalam list kartu
 void PrintKartuList(KartuList *head);
+// Fungsi untuk mencetak list kartu
 
 #endif
